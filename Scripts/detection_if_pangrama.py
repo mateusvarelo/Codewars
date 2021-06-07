d9fca5ef178fa562c7d175e8234a9a1c82b1d1ee
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-07 12:11:44 -0300

    implentando lista

--- Scripts/detection_if_pangrama.py
+++ Scripts/detection_if_pangrama.py
@@ -2,13 +2,11 @@ import string
 
 
 def is_pangram(s):
-    caracteres = list(
-                      "abcdefghijklmnopqrstuvwxyz!,"
-                     )
-   
-    for letra in s.replace(" ",""):                              
-        if letra.lower()   in caracteres:
-            return False
-    return True
-pangram = "The quick, brown fox jumps over the lazy dog!"
+    caracteres =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!',',']
+    lista = list(s.replace(" ","").lower())
+    for letra in lista:                             
+        if letra not in caracteres:
+            return True
+    return False
+pangram = "The quick, brown fox jumps over the lay dog!"
 print(is_pangram(pangram))
