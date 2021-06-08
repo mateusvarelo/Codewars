1f2f7c1ded0cc99960a16bc629eb1c87d60710f1
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-08 11:49:05 -0300

    find_needle

---
+++ Scripts/codewars_find_needle.py
@@ -0,0 +1,12 @@
+# Can you find the needle in the haystack?
+
+# Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+# After your function finds the needle it should return a message (as a string) that says:
+
+# "found the needle at position " plus the index it found the needle, so: 
+def find_needle(haystack):
+    # your code here
+ return f"found the needle at position {haystack.index('needle')}"
+teste = ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']
+print(find_needle(teste))
