e59983234c6ba4570b19500170a40aec4bda7c42
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-16 10:49:31 -0300

    terminando dia 15

--- Scripts/codewars15.py
+++ Scripts/codewars15.py
@@ -1,16 +1,10 @@
 
 def open_or_senior(data):
-    
-    dados = list(map(lambda x:list(x),data))
-    data.clear()
-    # data = [ 
-    #               "Senior" if idade[0] > 55 and 
-    #               handicap[1] > 7 else "Open"
-                  
-    # ]
-    for i,idade in enumerate(dados):
-         for j in range(len(idade)):
-             print(idade[j])
-    return data
-
+    dados = [
+            'Senior'if (x[0] >= 55) and (x[1] > 7)    
+             else 'Open'
+             for i,x in enumerate(data)         
+    ]
+           
+    return dados
 print(open_or_senior([(45, 12),(55,21),(19, -2),(104, 20)]))
