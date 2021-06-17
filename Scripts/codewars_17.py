6eb69351ae2df2b168c26b233e849b77640e0371
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-17 12:21:43 -0300

    em processo

--- Scripts/codewars_17.py
+++ Scripts/codewars_17.py
@@ -9,12 +9,16 @@ multiply the digits in num until you reach a single digit.
 def persistence(n):
      lista = list(str(n))
      lista = list(map(lambda x:int(x),lista))
+     if len(lista) == 0
      i = 1
-     while i <= len(lista):
-            
+     while i != len(lista):
+          lista[i-1]*
+          i+= 1
+          
                  
-      
-     return 0
+     else:
+         return 0
+     
          
          
 print(persistence(44))     
