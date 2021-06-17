81ce44dfc02ebcc059df00c141a043dd806e1730
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-17 12:13:32 -0300

    Dia 17

---
+++ Scripts/codewars_17.py
@@ -0,0 +1,20 @@
+"""
+Write a function, persistence,
+that takes in a positive parameter num and returns
+its multiplicative persistence, which is the number of times you must 
+multiply the digits in num until you reach a single digit.
+"""
+
+
+def persistence(n):
+     lista = list(str(n))
+     lista = list(map(lambda x:int(x),lista))
+     i = 1
+     while i <= len(lista):
+            
+                 
+      
+     return 0
+         
+         
+print(persistence(44))     
