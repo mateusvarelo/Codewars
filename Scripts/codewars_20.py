13476341a03d317e977cddc12c369b9ccbac19a3
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-20 11:29:46 -0300

    usando sorted

--- Scripts/codewars_20.py
+++ Scripts/codewars_20.py
@@ -8,13 +8,7 @@ The input to
 the function will be an array of 
 three distinct numbers (Haskell: a tuple)."""
 
-def gimme(input_array):
+def gimme(inputArray):
     # Implement this function
-    for i,valor in enumerate(input_array):
-        if (min(input_array) == valor):
-             continue 
-        elif (max(input_array)== valor):  
-             continue
-        else:
-             return i       
-print(gimme([5,10,14]))        
+    return inputArray.index(sorted(inputArray)[1])      
+print(gimme([6,5,14]))        
