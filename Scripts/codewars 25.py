4f892a1f3a897a700aacccc343ba69d7a3459ad4
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-25 11:36:53 -0300

    dia 25.2

--- Scripts/codewars 25.py
+++ Scripts/codewars 25.py
@@ -6,11 +6,8 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 """
 def get_count(input_str):
-    num_vowels = 0
+     q = sum(c in 'aeiou' for c in input_str)
     # your code here
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    for letra in input_str:
-        if letra.lower() in vowels:
-            num_vowels+=1
-    return num_vowels
+     print(q)
+     return sum(1 for let in input_str if let in "aeiouAEIOU")
 print(get_count("o  kak ushakov lil vo kashu kakao"))
