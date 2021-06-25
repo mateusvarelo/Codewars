bac1773a4a1f33b822e312263dfa678197a7108c
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-25 11:30:56 -0300

    Dia 25.1

---
+++ Scripts/codewars 25.py
@@ -0,0 +1,16 @@
+
+"""Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+"""
+def get_count(input_str):
+    num_vowels = 0
+    # your code here
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for letra in input_str:
+        if letra.lower() in vowels:
+            num_vowels+=1
+    return num_vowels
+print(get_count("o  kak ushakov lil vo kashu kakao"))
