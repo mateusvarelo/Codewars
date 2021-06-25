630cf3cc32a4165eef11e53e8d221f2906e1c139
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-25 11:22:27 -0300

    Dia 25

--- Scripts/codewars_21.1.py
+++ Scripts/codewars_21.1.py
@@ -4,9 +4,6 @@ string of space separated numbers,
 and have to return the highest and lowest number.
 """
 def high_and_low(numbers):
-    # ...
     numbers = numbers.replace(" ","")
-    print(numbers)
-    
     return f"{max(list(numbers))} {min(list(numbers))}"
-print(high_and_low("-1  -1"))
+print(high_and_low("1 0 -1"))
