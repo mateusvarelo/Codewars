614bdc661a1ba86a2eab878fda4e51cb6f348941
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-27 10:46:30 -0300

    Dia 27

--- Scripts/codewars27.py
+++ Scripts/codewars27.py
@@ -5,9 +5,9 @@ when determining if a character is a duplicate.
 """
 def duplicate_encode(word):
     #your code here
-    word = list(word.lower())
-    parentese = [ ')' if word.count(i) > 1 else '(' 
-                 for i in word]   
-    return ''.join(parentese)         
-print(duplicate_encode('recede'))    
+    word = list(word.lower())  
+    return ''.join([
+                    ')' if word.count(i) > 1 else '(' 
+                    for i in word])         
+print(duplicate_encode('aaaaaAaaaa'))    
     
