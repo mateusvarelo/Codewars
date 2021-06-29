a03619863fb5c516f37057dbfa4646b6de6d8b77
Mateus Varelo <mateusferreira703@gmail.com>
2021-06-29 10:38:52 -0300

    dia 29

---
+++ Scripts/codewars29.py
@@ -0,0 +1,11 @@
+def sort_by_length(arr):
+     a = [palavra 
+          for i in arr
+          for j in range() 
+          ]
+     return arr
+print(
+    sort_by_length(
+                ['beg', 'i', 'life', 'to']
+                )
+    )
