9d5608cbb4362d54a1eba2506a59011fa5f549da
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-06 11:37:10 -0300

    dia 6.2

--- Scripts/codewars06.py
+++ Scripts/codewars06.py
@@ -1,9 +1,8 @@
 def in_array(array1, array2):
     # your code
-    r = [
+    return list(set([
             pal for pal in array1
             for pal2 in array2
             if pal2.find(pal) != -1
-    ]
-    return list(set(r))
+    ])
 print(in_array(["arp", "mice", "bull"]  ,["lively", "alive", "harp", "sharp", "armstrong"]))
