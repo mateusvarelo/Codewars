b08c7d7753e90feb1ba8970307dcaa7a0951dd5a
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-09 13:28:58 -0300

    dia9.2

--- Scripts/codewars8.py
+++ Scripts/codewars8.py
@@ -1,7 +1,6 @@
 def to_weird_case(string='This is a test'):
     nova_string = [let.upper() if i % 2 == 0 else let.lower()
                    for i,let in enumerate(string)
-                 
                   ]
     print(nova_string)
     return ''.join(nova_string) 
