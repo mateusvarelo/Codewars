81893cff92630d53841a5bbeb10f9448f2c1a559
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-11 13:45:53 -0300

    dia11.3

--- Scripts/codewars10.1.py
+++ Scripts/codewars10.1.py
@@ -1,6 +1,6 @@
 def calculate_string(st): 
     clear_string = ''
-    operadores = ['+',' -', '*','/']
+    operadores = ['+','-', '*','/']
     numeros = [str(i) for i in range(10)]
     for letra in st:
       if (letra in numeros) or (letra in operadores) or (letra == '.'):
@@ -8,5 +8,12 @@ def calculate_string(st):
     for let in clear_string:
           if let in operadores:
               lista = clear_string.split(let)  
-              return f'{float(lista[0])}{let}{float(lista[1])}'      
-print(calculate_string("gdfgdf2.34dg54gf*23oP42"))
+              if let == '*':   
+                 return  str(int(float(lista[0])*float(lista[1])))
+              elif let == '+':
+                 return  str(int(float(lista[0])+float(lista[1])))
+              elif let == '/':
+                 return  str(int(float(lista[0])/float(lista[1])))
+              elif let == '-':
+                 return  str(int(float(lista[0])-float(lista[1])))        
+print(calculate_string("fsdfsd235???34.4554s4234df-sdfgf2g3h4j442"))
