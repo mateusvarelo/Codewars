868b3afb6ae4926a0029a24bab8445c822a01f38
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-12 13:12:39 -0300

    dia12

---
+++ Scripts/codewars11.py
@@ -0,0 +1,16 @@
+def twos_difference(lst): 
+    # your code here
+    tm = len(lst)
+    lista = []
+    for i in range(tm):
+        for j in range(tm):
+           try:
+              dif = lst[i]-lst[j+1]
+              if dif == -2 or dif == 2:
+                  lista = set(sorted(lista.append(sorted(lst[i],lst[j+1]))))
+           except:
+              print()  
+    return lista   
+                
+                   
+print(twos_difference([1, 2, 3, 4])) 
