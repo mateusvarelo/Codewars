84f1725624900a28b267393a4980066c9a6ce86a
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-13 13:09:36 -0300

    dia13

--- Scripts/codewars11.py
+++ Scripts/codewars11.py
@@ -7,9 +7,10 @@ def twos_difference(lst):
            try:
               dif = lst[i]-lst[j+1]
               if dif == -2 or dif == 2:
-                  lista = set(sorted(lista.append(sorted(lst[i],lst[j+1]))))
+                  lista.append((lst[i],lst[j+1]))
            except:
               print('')  
-    return lista   
+    lista  = list((map(tuple,list(map(sorted,lista)))))         
+    return sorted(set(lista))  
                    
 print(twos_difference([1, 2, 3, 4])) 
