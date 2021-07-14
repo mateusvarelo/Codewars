5bbf77086331e781f15320e391f85a20a699be26
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-14 13:01:15 -0300

    dia 14

--- Scripts/codewars13.1.py
+++ Scripts/codewars13.1.py
@@ -1,11 +1,8 @@
 def duplicate_encode(word):
     #your code here
-    tm =len(word.split())-1
-    lista = word.split()
-    print(lista[tm])
     return ''.join(
                   [
-                   '(' if word.count(letra)<2 else ')'  
-                    for letra in lista[tm]] 
+                   '(' if word.upper().count(letra)<2 else ')'  
+                    for letra in word.upper()] 
  )  
-print(duplicate_encode("aaa aaavb wI(GzSG@)FOdyRQwau(a"))
+print(duplicate_encode("wI(GzSG@)FOdyRQwau(a"))
