a53cfa119765ab10e01f8c51c20618e9a2b23a2b
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-15 10:32:42 -0300

    dia 15

--- Scripts/codewars_17.py
+++ Scripts/codewars_17.py
@@ -4,13 +4,18 @@ that takes in a positive parameter num and returns
 its multiplicative persistence, which is the number of times you must 
 multiply the digits in num until you reach a single digit.
 """
-
-
 def persistence(n):
-     if len(n) == 1:
-          return 0
-     else:
-          soma = list(map(lambda x,y:x*y,list(str(n)) ))
-         
-         
+    # your
+    cont = 0
+    n = list(str(n))
+    if len(n) != 1:
+      while len(n)!=1:
+           print(n)
+           n = numpy.prod(list(map(int,n))) 
+           n = list(str(n)) 
+           cont +=1
+      else:
+           return cont
+    else:
+      return 0     
 print(persistence(44))     
