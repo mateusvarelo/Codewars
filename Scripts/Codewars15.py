9d613b5492521770c29a620ab8ae090994ffaa7b
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-16 11:43:35 -0300

    dia 16

--- Scripts/Codewars15.py
+++ Scripts/Codewars15.py
@@ -1,5 +1,31 @@
+import re
 class Calculator(object):
+  
   def evaluate(self, string):
-           return f'{string[0::]}'
-c = Calculator()
-print(c.evaluate("2 / 2 + 3 * 4 - 6"))
+      listaNumeros = []
+      listaNum = []
+      listaOp = []
+
+      listaNum = re.findall(r'\d.?\d', string)
+      for i in listaNum:
+        listaNumeros.append(float(i))
+
+      listaOp.append('+')
+      for op in string:
+        if op.isspace() == False and op.isnumeric() == False and op != '.': 
+          listaOp.append(op)
+      resultado = []
+
+      for n, op in zip(listaNumeros, listaOp): 
+        if op == '/':
+          resultado.append(resultado.pop() / n)
+        elif op == '':
+          resultado.append(resultado.pop() * n)
+        elif op == '+':
+          resultado.append(n)
+        elif op == '-':
+          resultado.append(-n)
+
+      return sum(resultado)
+num = Calculator()    
+print(num.evaluate("2 / 2 + 3 * 4 - 6"))
