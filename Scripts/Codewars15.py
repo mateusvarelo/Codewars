50685982d0a448eb689edbf4b9602db0e3daf757
Mateus Varelo <mateusferreira703@gmail.com>
2021-07-16 12:15:14 -0300

    dia 16.3

--- Scripts/Codewars15.py
+++ Scripts/Codewars15.py
@@ -3,28 +3,30 @@ class Calculator(object):
   
   def evaluate(self, string):
       listaNum = []
-      listaNumeros = [
+      lista_Numeros = [
                       float(i) for i in string
                       if i.isdigit()
       ]
-      lista_Op+= '+'
       lista_Operadores = [ operador
                           for operador in string
                           if not operador.isspace()  and not operador.isnumeric()  and operador != '.'
       ]
-      
-           resultado = []
+      #lista_Operadores.insert(0,'+')
+      print(lista_Operadores)
+      resultado = []
 
-      for n, op in zip(listaNumeros, listaOp): 
-        if op == '/':
-          resultado.append(resultado.pop() / n)
-        elif op == '':
-          resultado.append(resultado.pop() * n)
-        elif op == '+':
-          resultado.append(n)
-        elif op == '-':
-          resultado.append(-n)
-
+      for num, op in zip(lista_Numeros, lista_Operadores): 
+        try:
+          if op == '/':
+            resultado.append(resultado.pop() / num)
+          elif op == '':
+            resultado.append(resultado.pop() * num)
+          elif op == '+':
+            resultado.append(num)
+          elif op == '-':
+            resultado.append(-num)
+        except:
+             continue
       return sum(resultado)
 num = Calculator()    
 print(num.evaluate("2 / 2 + 3 * 4 - 6"))
