00398f31c207268cc6b03ab4c8179486f1426563
Mateus Varelo <mateusferreira703@gmail.com>
2021-08-03 13:16:38 -0300

    atualizando

--- Scripts/codewars03/08.py
+++ Scripts/codewars03/08.py
@@ -1,22 +1,13 @@
-import string
+
 def pig_it(text):
     #your code here
-    letra = 'ay'
-    lista_palavras = text.split(' ') 
-    lista = []
-    for pal in lista_palavras:
-        primeira_letra = list(pal)[0].lower()
-        if primeira_letra in list(string.ascii_lowercase):
-            primeira_letra = list(pal)[0]
-            lista_letras = list(pal)
-            lista_letras.append(primeira_letra)
-            lista_letras.append(letra)
-            lista_letras.pop(0)
-        
-            lista.append(''.join(lista_letras))
-        else: 
-            lista.append(primeira_letra)
-                
-    return ' '.join(lista)
+    lst = text.split()
+    return (
+         ' '.join(
+             [word[1:] + word[:1]
+             + 'ay' if word.isalpha() else word 
+             for word in lst]
+             )
+)
 
-print(pig_it('Pig latin is cool '))
+print(pig_it('Pig latin is cool ! '))
