45d6a456047b9aee645cba09c7c97b639d09184f
Mateus Varelo <mateusferreira703@gmail.com>
2021-08-03 12:45:52 -0300

    Dia 03

---
+++ Scripts/codewars03/08.py
@@ -0,0 +1,19 @@
+import string
+def pig_it(text):
+    #your code here
+    letra = 'ay'
+    lista_palavras = text.split(' ') 
+    lista = []
+    for pal in lista_palavras:
+        primeira_letra = list(pal)[0].lower()
+        if primeira_letra in list(string.ascii_lowercase):
+            primeira_letra = list(pal)[0]
+            lista_letras = list(pal)
+            lista_letras.append(primeira_letra)
+            lista_letras.append(letra)
+            lista_letras.pop(0)
+        
+        lista.append(''.join(lista_letras))
+    return ' '.join(lista)
+
+print(pig_it('Pig latin is cool !'))
