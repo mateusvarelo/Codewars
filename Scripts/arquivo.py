91eaf0f15be442e421d85720602226e473dca72e
Mateus Varelo <mateusferreira703@gmail.com>
2021-08-03 12:16:25 -0300

    dia 03

--- Scripts/arquivo.py
+++ Scripts/arquivo.py
@@ -1,7 +1,7 @@
-arqui = open('/home/mateus/Área de Trabalho/Rotinas/julho','w')
+arqui = open('/home/mateus/Área de Trabalho/Rotinas/Agosto.txt','w')
 arqui.write(
     'Rotina:\n'        
-             '                      [Duolingo] - [Leitura]   -   [Commit]  -  [codewars]  - [Alongar-se]\n')
+             '                      [Duolingo] - [Leitura]   -   [Commit]  -  [codewars]  - [Alongar-se] - [Aerobico-Atividade]\n')
 for i in range(31):
-    arqui.write(f'{i+1} de Julho de 2021        []          []             []            []             []\n')
+    arqui.write(f'{i+1} de Agosto de 2021        []          []             []            []             []         []\n')
 arqui.close()    
